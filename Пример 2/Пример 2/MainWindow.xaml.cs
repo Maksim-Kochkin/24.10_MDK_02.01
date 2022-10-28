1937a521fa9ac437d596e4d451201e7bb12f4915
Maksim-Kochkin <106807060+Maksim-Kochkin@users.noreply.github.com>
2022-10-28 17:28:11 +0500

    Add files via upload

---
+++ Пример 2/Пример 2/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Пример_2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+            InfoTb.DataContext = new List<object>() { "My text" };
+        }
+
+        private void btn2_Click(object sender, RoutedEventArgs e)
+        {
+            InfoTb.DataContext = 123;
+        }
+
+        private void btn3_Click(object sender, RoutedEventArgs e)
+        {
+            InfoTb.DataContext = new List<object>() { DependencyProperty.UnsetValue };
+        }
+
+        private void btn1_C_Click(object sender, RoutedEventArgs e)
+        {
+            InfoTb1.DataContext = "My text";
+        }
+
+        private void btn2_C_Click(object sender, RoutedEventArgs e)
+        {
+            InfoTb1.DataContext = null;
+        }
+
+        private void btn3_C_Click(object sender, RoutedEventArgs e)
+        {
+            InfoTb1.DataContext = 123;
+        }
+    }
+}
---
+++ Пример 3/Пример 3/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Пример_3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Preset1Btn_Click(object sender, RoutedEventArgs e)
+        {
+            TargetStack.DataContext = new List<object>() { "First value", "Second value", "Third value" };
+        }
+
+        private void Value_Preset2_Click(object sender, RoutedEventArgs e)
+        {
+            TargetStack.DataContext = new List<object>() { "Index: 0", "Index: 1", "Index: 2" };
+        }
+
+        private void Value_Preset3_Click(object sender, RoutedEventArgs e)
+        {
+            TargetStack.DataContext = new List<object>() { "Like", "it,", "please!" };
+        }
+    }
+}
